de23ee3aeead7da22284a9a952cd1ff8f4b15338
Joe Larson <joe@joelarson.com>
2014-06-27 12:18:29 -0700

    attribution, contact changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -181,7 +181,7 @@ var attributionControl = L.control({
 });
 attributionControl.onAdd = function (map) {
   var div = L.DomUtil.create("div", "leaflet-control-attribution");
-  div.innerHTML = "Developed by <a href='http://bryanmcbride.com'>bryanmcbride.com</a> | <a href='#' onclick='$(\"#attributionModal\").modal(\"show\"); return false;'>Attribution</a>";
+  div.innerHTML = "Developed by <a href='http://slocountyfire.org'>SLO County Fire</a> | <a href='#' onclick='$(\"#attributionModal\").modal(\"show\"); return false;'>Attribution</a>";
   return div;
 };
 map.addControl(attributionControl);
